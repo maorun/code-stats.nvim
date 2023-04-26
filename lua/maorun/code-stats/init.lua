8fcd8b409c9b8f92ad4f06d69f6c9dcfecbdbcfd
Marco Driemel <marco.driemel@axelspringer.com>
2023-04-26 17:10:13 +0200

    refactor(maorun/code-stats): extract request function feat(maorun/code-stats): add error handling for failed requests The request function is now extracted from the pulseSend function, which improves readability and maintainability. The request function is responsible for sending the POST request to the code-stats API. Error handling is added to the pulseSend function to handle failed requests. If the request fails, an error message is returned in the response body.

---
+++ lua/maorun/code-stats/init.lua
@@ -0,0 +1,101 @@
+local curl = require("plenary.curl")
+local pulse = require('maorun.code-stats.pulse')
+
+local defaults = {
+    status_prefix = 'C:S ',
+    api_url = 'https://codestats.net/',
+    api_key = '',
+}
+local config = defaults
+
+local error = ''
+local function setup(user_config)
+    local globalConfig = {}
+    if (vim.g.codestats_api_key) then
+        globalConfig.api_key = vim.g.codestats_api_key
+    end
+    config = vim.tbl_deep_extend("force", defaults, user_config or {}, globalConfig)
+    return config
+end
+
+local function currentXp()
+    if ( string.len(error) > 0) then
+        return config.status_prefix .. 'ERR'
+    end
+
+    return config.status_prefix .. pulse.getXp(vim.bo.filetype)
+end
+
+local function requestToApi(body)
+    local url = config.api_url
+
+    return curl.request({
+        url = url .. 'api/my/pulses/',
+        method = "POST",
+        headers = {
+            ["X-API-Token"] = config.api_key,
+            ["Content-Type"] = "application/json",
+            ["Accept"] = "application/json"
+        },
+        body = body,
+        on_error = function(data)
+            error = "could not send request to code-stats: " .. data.message
+        end,
+        callback = function()
+            error = ''
+            pulse.reset()
+        end,
+    })
+end
+
+local function pulseSend()
+
+    if (string.len(table.concat(vim.tbl_values(config))) == 0) then
+        error = config.status_prefix .. 'Not Initialized'
+    end
+
+    local url = config.api_url
+    if (string.len(url) == 0) then
+        error = 'no API-URL given'
+    end
+    if (string.len(config.api_key) == 0) then
+        error = 'no api-key given'
+    end
+
+    local languages = vim.fn.map(pulse.xps, function(language, xp)
+        if (xp > 0) then
+            return '{"language": "' .. language .. '", "xp": ' .. xp .. '}'
+        else
+            return ''
+        end
+    end)
+
+    local xps = table.concat(vim.tbl_values(languages), ',')
+
+    if (string.len(xps) > 0) then
+        requestToApi('{ "coded_at": "' .. os.date("%Y-%m-%dT%X%z") .. '", "xps": [ ' .. xps .. ' ] }')
+    end
+end
+
+vim.cmd [[
+    augroup codestats_track
+        autocmd!
+        autocmd InsertCharPre,TextChanged * lua require('maorun.code-stats').add(vim.bo.filetype)
+        autocmd VimLeavePre * lua require('maorun.code-stats').pulseSend()
+        autocmd BufWrite,BufLeave * lua require('maorun.code-stats').pulseSend()
+    augroup END
+]]
+
+function add(filetype)
+    pulse.addXp(filetype, 1)
+end
+
+return {
+    setup = setup,
+    add = add,
+    pulseSend = pulseSend,
+    currentXp = currentXp,
+    getError = function()
+        return error
+    end,
+}
---
+++ lua/maorun/code-stats/pulse.lua
@@ -0,0 +1,20 @@
+local pulse = {
+    xps = {}
+}
+
+pulse.addXp = function(lang, amount)
+    pulse.xps[lang] =  pulse.getXp(lang) + amount
+end
+
+pulse.getXp = function(lang)
+    if (pulse.xps[lang]) then
+        return pulse.xps[lang]
+    end
+    return 0
+end
+
+pulse.reset = function()
+    pulse.xps = {}
+end
+
+return pulse
