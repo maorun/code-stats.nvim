f00d8d0df9dc51f3e385ee2c01ff66712c051d61
Marco Driemel <maorun@users.noreply.github.com>
2025-07-08 15:29:31 +0200

    fix(config): global api-key not founded (#11)

--- lua/maorun/code-stats/config.lua
+++ lua/maorun/code-stats/config.lua
@@ -5,16 +5,18 @@ local defaults = {
 }
 local config = defaults
 
-local function setup(user_config)
+local M = {
+	config = vim.deepcopy(defaults),
+}
+
+function M.setup(user_config)
 	local globalConfig = {}
 	if vim.g.codestats_api_key then
 		globalConfig.api_key = vim.g.codestats_api_key
 	end
 	config = vim.tbl_deep_extend("force", defaults, user_config or {}, globalConfig)
+	M.config = config
 	return config
 end
 
-return {
-	setup = setup,
-	config = config,
-}
+return M
